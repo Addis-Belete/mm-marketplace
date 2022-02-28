b5b16ec625a97594a2b946643d725bb243a4b491
Addis Belete <addis.bel@gmail.com>
2022-02-28 16:45:59 +0300

    Add a style to marketPlace component

---
+++ components/MarketPlace.jsx
@@ -0,0 +1,39 @@
+import { data } from "../data/data";
+
+const MarketPlace = () => {
+  return (
+    <div className="flex ">
+      <div className="flex-1 mr-5 mt-20 px-10">
+        {data.map((nft, i) => {
+          return (
+            <img
+              key={i}
+              src={nft.image}
+              alt={nft.description}
+              className={`${i == 0 ? "block" : "hidden"}`}
+            />
+          );
+        })}
+      </div>
+      <div className=" flex-1 grid grid-cols-2 gap-x-5 gap-y-10  pr-5 hover:shadow-lg">
+        {data.map((nft, i) => {
+          return (
+            <div key={i} className={`relative `}>
+              <img src={nft.image} alt={nft.description} className=" h-72" />
+              <div className=" absolute top-60 w-full">
+                <p className="text-white float-right px-2 text-active-1 font-bold">
+                  Price {nft.price} ETH
+                </p>
+                <button className="text-white w-full bg-button-1 py-2 text-lg">
+                  Buy
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default MarketPlace;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-
+import MarketPlace from "../components/MarketPlace"
 export default function Home() {
 	return (
 
-		<div className="text-red-200 mt-10 ml-10" >
-			Hello This is  market place am building for MMCY Company
+		<div  >
+			<MarketPlace />
 		</div>
 	)
 }
