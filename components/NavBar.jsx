7cf659d85d2f8af3d78d077aaa3837bcb4c88c7f
Addis Belete <addis.bel@gmail.com>
2022-02-28 12:29:04 +0300

    Add a dummy data

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -6,20 +6,20 @@ const NavBar = () => {
       <h3 className="text-white text-3xl font-bold">NFT's</h3>
       <div>
         <Link href="/">
-          <a className="text-white-1 text-xl mr-10 visited:text-active-1">Marketplace</a>
+          <a className="text-white-1 text-lg mr-10 visited:text-active-1">Marketplace</a>
         </Link>
         <Link href="/nfts-sold">
-          <a className="text-white-1 text-xl mr-10 visited:text-active-1">Post NFTs</a>
+          <a className="text-white-1 text-lg mr-10 visited:text-active-1">Post NFTs</a>
         </Link>
         <Link href="/sold_items">
-          <a className="text-white-1 text-xl mr-10 visited:text-active-1">Sold NFTs</a>
+          <a className="text-white-1 text-lg mr-10 visited:text-active-1">Sold NFTs</a>
         </Link>
         <Link href="/your_nfts">
-          <a className="text-white-1 text-xl">Your NFTs</a>
+          <a className="text-white-1 text-lg">Your NFTs</a>
         </Link>
       </div>
 
-      <button className="text-white bg-button-1 py-3 px-10">Connect</button>
+      <button className="text-white text-lg bg-button-1 py-2 px-10">Connect</button>
     </div>
   );
 };
---
+++ data/data.js
@@ -0,0 +1,35 @@
+export const data = [
+	{
+		price: 1,
+		image: "",
+		description: "",
+		owned: "",
+		seller: "",
+		sold: false,
+	},
+	{
+		price: 1,
+		image: "",
+		description: "",
+		owned: "",
+		seller: "",
+		sold: false,
+	},
+	{
+		price: 1,
+		image: "",
+		description: "",
+		owned: "",
+		seller: "",
+		sold: false,
+	},
+	{
+		price: 1,
+		image: "",
+		description: "",
+		owned: "",
+		seller: "",
+		sold: false,
+	}
+
+]
