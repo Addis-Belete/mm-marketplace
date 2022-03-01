3306cd72ed5f20f2069a98ca3e8d368aef4b5e68
Addis Belete <addis.bel@gmail.com>
2022-03-01 15:07:43 +0300

    Add Market.sol and import openzepplin

---
+++ contracts/Market.sol
@@ -0,0 +1,5 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity >=0.4.22 <0.9.0;
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
