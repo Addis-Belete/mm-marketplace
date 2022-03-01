ea3691b15d477c4f1e87706fb2d110f568e7565e
Addis Belete <addis.bel@gmail.com>
2022-03-01 16:05:39 +0300

    Add getMarketOwner function

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -3,3 +3,40 @@ pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "hardhat/console.sol";
+
+contract Market is ReentrancyGuard {
+    using Counters for Counters.Counter;
+    Counters.Counter private _itemsIds;
+    Counter.Counter private _itemsSold;
+    address payable owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    struct MarketItem {
+        uint256 itemId;
+        address nftContract;
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+    }
+    mapping(uint256 => MarketItem) private idToMarketItem;
+    event MarketItemCreated(
+        uint256 indexed itemId,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+
+    function getMarketOwner() public view returns (address) {
+        return owner;
+    }
+    // places an item for sale on market place
+}
