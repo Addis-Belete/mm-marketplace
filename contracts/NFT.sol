7912daa8cd9d494d12ad12ea54f6956122857beb
Addis Belete <addis.bel@gmail.com>
2022-03-01 14:15:51 +0300

    Add a owner only modifier

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -4,3 +4,23 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
+
+contract NFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    address contractAddress;
+
+    constructor(address marketPlaceAddress) ERC721("ADD NFTS", "ADD") {
+        contractAddress = marketPlaceAddress;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function createNFT(string memory tokenURI) public returns (uint256) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+    }
+}
