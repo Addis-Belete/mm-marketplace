43c56b131d03361fb0fb0d31e73a51410cbe1c01
Addis Belete <addis.bel@gmail.com>
2022-03-01 13:58:22 +0300

    Import openzepplin contracts

---
+++ contracts/NFT.sol
@@ -0,0 +1,6 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity >=0.4.22 <0.9.0;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
